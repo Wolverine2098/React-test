1323409a6285e840ccff3397a68d6310a98aaef2
Raunak <raunak.goswami@indiamart.com>
2024-05-23 22:29:32 +0530

    first commit test

---
+++ App.js
@@ -0,0 +1,12 @@
+const element = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello world"
+);
+const h1 = React.createElement("h1", { id: "child2" }, "I am h1");
+const h2 = React.createElement("h2", { id: "child2" }, "I am h2");
+const child1 = React.createElement("div", { id: "child1" }, [h1, h2]);
+const child2 = React.createElement("div", { id: "child2" }, [h1, h2]);
+const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
