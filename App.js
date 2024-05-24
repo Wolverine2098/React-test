ad4b755e281d9d0afa24f9fd9293516a30370242
Raunak <raunak.goswami@indiamart.com>
2024-05-25 00:22:08 +0530

    jsx usage

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello world"
-);
-const h1 = React.createElement("h1", { id: "child2" }, "I am h1");
-const h2 = React.createElement("h2", { id: "child2" }, "I am h2");
-const child1 = React.createElement("div", { id: "child1" }, [h1, h2]);
-const child2 = React.createElement("div", { id: "child2" }, [h1, h2]);
-const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+// const element = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello world"
+// );
+// const h1 = React.createElement("h1", { id: "child2" }, "I am h1");
+// const h2 = React.createElement("h2", { id: "child2" }, "I am h2");
+// const child1 = React.createElement("div", { id: "child1" }, [h1, h2]);
+// const child2 = React.createElement("div", { id: "child2" }, [h1, h2]);
+// const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+const elementJSX = <h1>"This is HTML like syntax to learn fundamentals"</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(elementJSX);
