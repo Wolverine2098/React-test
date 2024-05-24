138ec3d3af514507c77375f2ad141cb3fb257b16
Raunak <raunak.goswami@indiamart.com>
2024-05-25 01:10:39 +0530

    component compounding

--- App.js
+++ App.js
@@ -11,6 +11,18 @@ import ReactDOM from "react-dom";
 // const child1 = React.createElement("div", { id: "child1" }, [h1, h2]);
 // const child2 = React.createElement("div", { id: "child2" }, [h1, h2]);
 // const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
+const ComponentJSX2 = () => {
+  return <h1>This is second component</h1>;
+};
+
+const ComponentJSX = () => {
+  return (
+    <div id="container">
+      <ComponentJSX2 />
+      <h1>This code is from primary Component</h1>
+    </div>
+  );
+};
 const elementJSX = <h1>"This is HTML like syntax to learn fundamentals"</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(elementJSX);
+root.render(<ComponentJSX />);
