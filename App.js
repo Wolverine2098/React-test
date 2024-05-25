50a643efa3fec6354b3d0a88fa49c2ac60d6285d
Raunak <raunak.goswami@indiamart.com>
2024-05-25 20:44:24 +0530

    making the card

--- App.js
+++ App.js
@@ -21,8 +21,17 @@ const Header = () => {
 
 const RestaurentCard = () => {
   return (
-    <div className="res-card">
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/64fd45fd9f44c1737bc446e470bed666"
+      />
       <h3>Meghna Foods</h3>
+      <h5>Biryani, North Indian, chinese</h5>
+      <h5>4.4 stars</h5>
+      <h5>ETA 38 mins</h5>
+      <h5>Rs 750 for 2</h5>
     </div>
   );
 };
