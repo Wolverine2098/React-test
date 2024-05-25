f2ee2dc0652c497059b91a3ed9afdf96d1564716
Raunak <raunak.goswami@indiamart.com>
2024-05-25 20:31:12 +0530

    preparing wireframe

--- App.js
+++ App.js
@@ -1,28 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// const element = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello world"
-// );
-// const h1 = React.createElement("h1", { id: "child2" }, "I am h1");
-// const h2 = React.createElement("h2", { id: "child2" }, "I am h2");
-// const child1 = React.createElement("div", { id: "child1" }, [h1, h2]);
-// const child2 = React.createElement("div", { id: "child2" }, [h1, h2]);
-// const parent = React.createElement("div", { id: "parent" }, [child1, child2]);
-const ComponentJSX2 = () => {
-  return <h1>This is second component</h1>;
-};
-
-const ComponentJSX = () => {
+import logo from "./assets/foodIcon.png";
+const Header = () => {
   return (
-    <div id="container">
-      <ComponentJSX2 />
-      <h1>This code is from primary Component</h1>
+    <div id="Header">
+      <div className="logo-container">
+        <img className="logo" src={logo} />
+      </div>
+      <div className="list-Items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
-const elementJSX = <h1>"This is HTML like syntax to learn fundamentals"</h1>;
+
+const RestaurentCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghna Foods</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">search</div>
+      <div className="res-container"><RestaurentCard/></div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div id="App">
+      <Header />
+      <Body></Body>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ComponentJSX />);
+root.render(<AppLayout />);
