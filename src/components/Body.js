717d8929c79674cf1f741fed0bf1ab0da54e5b06
Raunak <raunak.goswami@indiamart.com>
2024-05-26 14:50:58 +0530

    add the filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,26 @@
 import resObj from "../utils/mockData";
 import RestaurentCard from "./RestaurantCard";
+import { useState } from "react";
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resObj);
   return (
     <div className="body">
-      <div className="Search">search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filteredList = resObj.filter(
+              (restaurant) => restaurant.info.avgRating > 4.2
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top rated restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resObj.map((restaurant) => (
-          <RestaurentCard resData={restaurant} />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurentCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
